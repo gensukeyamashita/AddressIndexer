5792daf3108d2ce728719fa6cee4fe3d755670bc
山下チアゴ <yamashiita@gmail.com>
2022-11-23 22:16:42 +0900

    検索出力まで完了

--- test/test_AddressIndexer.rb
+++ test/test_AddressIndexer.rb
@@ -8,11 +8,17 @@ class TestAddressIndexer < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::AddressIndexer::VERSION
   end
-
-  def test_it_does_something_useful
-    #AddressIndexer.loadCsvIntoListOfList
-    #AddressIndexer.printList
-    AddressIndexer.printListForUserInput('“Œ ‹ž “s')
-    #assert false
+  def test_step1
+    AddressIndexer.join_same_records '/test.csv'
   end
+  def test_step2
+    AddressIndexer.index_csv_file '/KEN_JOINED.csv'
+  end
+  def test_step3
+    AddressIndexer.create_ngram_index_file '/KEN_JOINED.csv'
+  end
+  def test_step4
+    AddressIndexer.search_using_ngram_and_index_files('”Ñ“c','/KEN_JOINED.csv')
+  end
+
 end
